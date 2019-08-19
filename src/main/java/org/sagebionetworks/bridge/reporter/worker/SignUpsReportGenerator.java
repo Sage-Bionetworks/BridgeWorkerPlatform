8d30bedbfb00b710955194f6236183be203f6551
Janae Chan <jchan@xschildws-MacBook-Pro.local>
2019-08-19 14:07:49 -0700

    Grab RequestInfo and ActivityEvents, and helper methods to mock data used in testing

--- src/main/java/org/sagebionetworks/bridge/reporter/worker/SignUpsReportGenerator.java
+++ src/main/java/org/sagebionetworks/bridge/reporter/worker/SignUpsReportGenerator.java
@@ -1,11 +1,14 @@
 package org.sagebionetworks.bridge.reporter.worker;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -13,6 +16,8 @@ import org.springframework.stereotype.Component;
 import org.sagebionetworks.bridge.reporter.helper.BridgeHelper;
 import org.sagebionetworks.bridge.reporter.request.ReportType;
 import org.sagebionetworks.bridge.rest.model.AccountStatus;
+import org.sagebionetworks.bridge.rest.model.ActivityEventList;
+import org.sagebionetworks.bridge.rest.model.RequestInfo;
 import org.sagebionetworks.bridge.rest.model.SharingScope;
 import org.sagebionetworks.bridge.rest.model.StudyParticipant;
 
@@ -49,6 +54,8 @@ public class SignUpsReportGenerator implements ReportGenerator {
         List<StudyParticipant> participants = bridgeHelper.getParticipantsForStudy(studyId, startDate, endDate);
         for (StudyParticipant participant : participants) {
             statuses.add(participant.getStatus());
+            System.out.println(participant.getCreatedOn() + " : " + participant.getStatus() + " : " + participant.getEmail());
+            
             // Accounts that aren't enabled do not have interesting sharing statuses. We'd like to count these
             // for consented accounts, but this isn't easy to do.
             if (participant.getStatus() == AccountStatus.ENABLED) {
@@ -72,10 +79,51 @@ public class SignUpsReportGenerator implements ReportGenerator {
         for (SharingScope scope : sharingScopes) {
             sharingData.put(scope.name().toLowerCase(), sharingScopes.count(scope));
         }
+        
         Map<String, Map<String,Integer>> reportData = new HashMap<>();
         reportData.put("bySharing", sharingData);
         reportData.put("byStatus", statusData);
 
+        	
+        // Total number of participants: last_sign_in vs days_in_study
+        Map<StudyParticipant, RequestInfo> requestInfos = bridgeHelper.getRequestInfoForParticipant(studyId, participants);
+        List<Integer> signInData = new ArrayList<>();
+        for (Entry<StudyParticipant, RequestInfo> entry : requestInfos.entrySet()) {
+        	if (entry.getValue().getSignedInOn() != null) { // Has signed in
+        		System.out.println("Participant: " + entry.getKey().getEmail());
+        		System.out.println("Created on: " + entry.getKey().getCreatedOn().toLocalDate() + " Signed in on: " + entry.getValue().getSignedInOn().toLocalDate());
+        		int days = Days.daysBetween(entry.getKey().getCreatedOn().toLocalDate(), entry.getValue().getSignedInOn().toLocalDate()).getDays();
+        		System.out.println("Days between: " + days);
+        		while (signInData.size() < (days + 1)) {
+            		signInData.add(0);
+            	}
+        		signInData.set(days, signInData.get(days) + 1);
+        	}
+        }
+        System.out.println("Sign In Data: " + signInData.toString());
+        
+        // Total number of participants: last_uploaded_on vs days_in_study
+        Map<RequestInfo, ActivityEventList> activityEventList = bridgeHelper.getActivityEventForParticipant(studyId, participants);
+        List<Integer> uploadedOnData = new ArrayList<>();
+        for (Entry<RequestInfo, ActivityEventList> entry : activityEventList.entrySet()) {
+        	if (entry.getKey().getSignedInOn() != null) { // Has signed in
+        		for (int i = 0; i < entry.getValue().getItems().size(); i++) {
+    	    		if (entry.getValue().getItems().get(i).getEventId().equals("activities_retrieved")) {
+    	    			System.out.println("Activity Retrieved  on: " + entry.getValue().getItems().get(i).getTimestamp().toDateTime().toLocalDate() + " Uploaded on: " + entry.getKey().getUploadedOn().toLocalDate());
+    	    			int days = Days.daysBetween(entry.getValue().getItems().get(i).getTimestamp().toDateTime().toLocalDate(), entry.getKey().getUploadedOn().toLocalDate()).getDays();
+    	        		System.out.println("Days between: " + days);
+    	        		while (uploadedOnData.size() < (days + 1)) {
+    	    				uploadedOnData.add(0);
+    	            	}
+    	    			uploadedOnData.set(days, uploadedOnData.get(days) + 1);
+    	    		}
+        		}
+        	}
+        }
+        System.out.println("Upload On Data: " + uploadedOnData.toString());
+//        reportData.put("bySignIn", signInData);
+//        reportData.put("byUploadedOn", uploadedOnData);
+        
         return new Report.Builder().withStudyId(studyId).withReportId(reportId).withDate(startDate.toLocalDate())
                 .withReportData(reportData).build();
     }
